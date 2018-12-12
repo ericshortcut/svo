56a7c53175700561afe7d49fa16fdd6a14e57648
Eric Andrade <eoandrade@stefanini.com>
2018-12-12 14:30:06 -0200

    Moving code to index

--- index.js
+++ index.js
@@ -1,102 +1,30 @@
-const SVO = require('./SVO');
+const PermissionEngine = require('./PermissionEngine');
+const foldEngine = require('./foldEngine');
 
-const facts = [{
-    fact: {
-        action: 'register_books',
-        label: 'Register books',
-        rules: [
-            "[subject.profile === 'system_adm']",
-            "[object.state === 'NY', subject.profile === 'warehouse_manager']"
-        ]
+module.exports.buildFacts = (facts)=>{
+    const indexActions = facts.reduce((base,item)=>{
+        base[item.fact.action] = {
+    
+            setSubjectAndObject: (subject,object)=>{
+                const permissionEng = new PermissionEngine({
+                    subject: subject,
+                    action: item.fact.action,
+                    object: object
+                })
+                return foldEngine(item.fact.rules.map(i=>eval(i)), permissionEng);
+            }
+        }
+        return base;
     }
-}, {
-    fact: {
-        action: 'register_users',
-        label: 'Register Users',
-        rules: ["[subject.profile === 'system_adm']"]
+    , {});
+
+    return {
+        setAction: (action)=>{
+            if (indexActions[action]){
+                return indexActions[action];
+            }else{
+                throw `Action (${action}) not found`;
+            }
+        }
     }
-}];
-
-(function testBookRegisteringByWareHouseManager() {
-    console.log("\n--------1st scenario--------\n")
-    const requestSimulationScene = {
-        subject: {
-            profile: 'warehouse_manager',
-            name: 'John Doe',
-            email: 'john.doe@company.com'
-        },
-        action: 'register_books',
-        object: {
-            ISBN: '123-123123',
-            name: 'The big adventures',
-            state: 'NY'
-        }
-    };
-    console.log(
-    SVO.buildFacts(facts)
-        .setAction(requestSimulationScene['action'])
-        .setSubjectAndObject(requestSimulationScene.subject, requestSimulationScene.object)
-        .then((s, v, o) => {
-            console.log(`\n>>> Book "${o.name}" was registered`, s, v, o);
-        }
-        ).fail((s, v, o) => {
-            console.log(`\n>>> User "${s.name}" does not have access. `);
-        }
-        )
-    )
-})();
-
-(function testBookRegisteringBySystemAdm() {
-    console.log("\n--------2nd scenario--------\n")
-    const requestSimulationScene = {
-        subject: {
-            profile: 'system_adm',
-            name: 'Mary Doe',
-            email: 'mary.doe@company.com'
-        },
-        action: 'register_books',
-        object: {
-            ISBN: '123-123123',
-            name: 'The big adventures',
-            state: 'NY'
-        }
-    };
-    SVO.buildFacts(facts)
-        .setAction(requestSimulationScene['action'])
-        .setSubjectAndObject(requestSimulationScene.subject, requestSimulationScene.object)
-        .then((s, v, o) => {
-            console.log(`\n>>> Book ${o.name} was registered`, s, v, o);
-        }
-        ).fail((s, v, o) => {
-            console.log(`\n>>> User "${s.name}" does not have access. `);
-        }
-        )
-})();
-
-
-(function testRegisterUserByWareHouseManager() {
-    console.log("\n--------3rd scenario--------\n")
-    const requestSimulationScene = {
-        subject: {
-            profile: 'warehouse_manager',
-            name: 'John Doe',
-            email: 'john.doe@company.com'
-        },
-        action: 'register_users',
-        object: {
-            profile: 'warehouse_manager',
-            name: 'Ramirez Silva',
-            email: 'ramirez.silva@company.com'
-        }
-    };
-    SVO.buildFacts(facts)
-        .setAction(requestSimulationScene['action'])
-        .setSubjectAndObject(requestSimulationScene.subject, requestSimulationScene.object)
-        .then((s, v, o) => {
-            console.log(`\n>>> User: ${o.name} was registered`, s, v, o);
-        }
-        ).fail((s, v, o) => {
-            console.log(`\n>>> User: "${s.name}" has no access rights to Register: ${o.name} `);
-        }
-        )
-})();
+};
